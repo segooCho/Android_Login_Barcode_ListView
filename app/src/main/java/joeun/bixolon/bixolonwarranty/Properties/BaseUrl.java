06c01856d0416015bae0ae9f42f0a8334ba19635
segooCho <admin@adminui-MacBook-Pro.local>
2017-04-26 19:06:13 +0900

    WorkList 1. Login : 작업 완료 - com.amitshekhar.android:android-networking : 작업 완료 - SSL Sample : 작업 완료
    2.Barcode Scan :
    - NavigationView
    - Xing
    - Get Information
    - Save Information
    
    3.ListView
    - get Information

---
+++ app/src/main/java/joeun/bixolon/bixolonwarranty/Properties/BaseUrl.java
@@ -0,0 +1,16 @@
+package joeun.bixolon.bixolonwarranty.Properties;
+
+/**
+ * Created by admin on 2017. 4. 25..
+ */
+
+public class BaseUrl {
+
+    //protected static final String Url = "http://192.168.2.30:3000/employees";
+    protected static final String Url = "http://192.168.2.30:3000/employees";
+
+    public String getUrl(){
+        return this.Url;
+    }
+
+}
