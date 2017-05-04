0cd2f986bf7348028f20d9d54523e9d1ce0a58d0
segooCho <admin@adminui-MacBook-Pro.local>
2017-05-04 21:17:54 +0900

    WorkList 1. Login : 작업 완료 - com.amitshekhar.android:android-networking : 작업 완료 - SSL Sample : 작업 완료
    2.Barcode Scan :
    - NavigationView : 작업 완료
    - Xing : 작업 완료
    - Get Information : 작업 완료
    - Save Information : 작업 완료
    - 소스 정리
    
    3.ListView
    - get Information

--- app/src/main/java/joeun/bixolon/bixolonwarranty/Activity/MainActivity.java
+++ app/src/main/java/joeun/bixolon/bixolonwarranty/Activity/MainActivity.java
@@ -163,56 +163,6 @@ public class MainActivity extends AppCompatActivity
             // TODO :: IntentIntegrator : REQUEST_CODE = 0x0000c0de;
             case 49374: {
                 new BarcodeEventOnActivityResult(this, result.getContents());
-                /*
-                textViewBarcode.setText("Barocde : " + result.getContents());
-                scanBarcode = null;
-                try {
-                    BaseUrl baseUrl = new BaseUrl();
-                    AndroidNetworking.post(baseUrl.getBarcodeUrl())
-                            .addBodyParameter("barcode",result.getContents())
-                            .setPriority(Priority.LOW)
-                            .build()
-                            .getAsJSONArray(new JSONArrayRequestListener() {
-                                @Override
-                                public void onResponse(JSONArray jsonArray) {
-                                    // do anything with responseUserLoginTask
-                                    Log.v("Barcode", "======================================");
-                                    Log.v("Barcode", "onResponse");
-                                    try{
-                                        for (int i = 0; i < jsonArray.length(); i++){
-                                            JSONObject jsonObject = jsonArray.getJSONObject(i);
-                                            //Log.v("Barcode", "LastName : " + jsonObject.getString("LastName"));
-                                            //Log.v("Barcode", "Email : " + jsonObject.getString("Email"));
-                                            scanBarcode = jsonObject.getString("FirstName");
-                                            textViewProductName.setText("ProductName : " + jsonObject.getString("LastName"));
-                                            textViewEmail.setText("E-Mail : " + jsonObject.getString("Email"));
-                                        }
-                                    }
-                                    catch (JSONException e){
-                                        Log.v("Barcode", "======================================");
-                                        Log.v("Barcode", "JSONException");
-                                        Log.v("Barcode", String.valueOf(e));
-                                        alertMessage.AlertShow("Error","Communication Error(JSONException)").show();
-                                    }
-                                }
-                                @Override
-                                public void onError(ANError error) {
-                                    // handle error
-                                    Log.v("Barcode", "======================================");
-                                    Log.v("Barcode", "ANError");
-                                    Log.v("Barcode", String.valueOf(error));
-                                    alertMessage.AlertShow("Error","Communication Error(ANError)").show();
-                                }
-                            });
-                    //TODO: 응답 시간을 강제적으로 기다린다... 추후 문제 발생 할수 있다.
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    Log.v("Barcode", "======================================");
-                    Log.v("Barcode", "InterruptedException");
-                    Log.v("Barcode", String.valueOf(e));
-                    alertMessage.AlertShow("Error","Communication Error(InterruptedException)").show();
-                }
-                */
             }
         }
     }
